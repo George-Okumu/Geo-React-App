a25490af49ea77988ad0b81e51952bbca66f5486
George Okumu <gokumu12@gmail.com>
2020-12-15 16:22:35 +0300

    introduced simple component instead of class component

--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,54 @@
 import React, {Component} from 'react'
+// Class Component
+// class Table extends Component{
+//     render (){
+//         return(
+//             <table>
+//             <thead>
+//               <tr>
+//                 <th>Name</th>
+//                 <th>Job</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               <tr>
+//                 <td>George</td>
+//                 <td>Junior Dev</td>
+//               </tr>
+//               <tr>
+//                 <td>Tom</td>
+//                 <td>Painter</td>
+//               </tr>
+//               <tr>
+//                 <td>Dee</td>
+//                 <td>Aspiring actress</td>
+//               </tr>
+//               <tr>
+//                 <td>Vivian</td>
+//                 <td>Scholar</td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         )
+//     }
+// }
 
-class Table extends Component{
-    render (){
-        return(
-            <table>
+
+
+
+// Introduction of Simple  components
+
+const TableHeader = () => {
+
+        return (
             <thead>
-              <tr>
-                <th>Name</th>
-                <th>Job</th>
-              </tr>
+                <tr>
+                    <th>Student</th>
+                    <th>Grade/Marks</th>
+                </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>George</td>
-                <td>Junior Dev</td>
-              </tr>
-              <tr>
-                <td>Tom</td>
-                <td>Painter</td>
-              </tr>
-              <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-              </tr>
-              <tr>
-                <td>Vivian</td>
-                <td>Scholar</td>
-              </tr>
-            </tbody>
-          </table>
         )
-    }
+
 }
 
 export default Table
