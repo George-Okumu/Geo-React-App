7e90d131f35f456dc0a50532380f774b0d7efc01
George Okumu <gokumu12@gmail.com>
2020-12-15 16:02:56 +0300

    Introduced App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>Hello, Welcome to George App</h1>
+        </div>
+      )
+    }
+  }
+
+  export default App
